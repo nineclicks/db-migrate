d59bffb81bef5db79a27e9569b56624ddfe638b2
Nick Gardner <nicholasg1989@gmail.com>
2017-10-19 14:13:33 -0700

    Add confirmation prompt

--- dothething.php
+++ dothething.php
@@ -1,12 +1,22 @@
 <?php
+
 include_once('db.php');
 include_once('order.php');
 include_once('driver.php');
 
 $mysql = connection("mysql","127.0.0.1","l43qdmlx_orders","root","");
 $pgsql = connection("pgsql","127.0.0.1","cvat","postgres","");
+
+echo "WARNING: This is going to clear your destination database. Type yes to continue.\n";
+$line = readLine(": ");
+if ($line != "yes") {
+  echo "Exiting with no changes.\n";
+  exit();
+}
+echo "Beginning migration.\n";
+
 $queries = parseQueries('queries.sql');
 
 clearDestDB($pgsql, $queries);
 doDrivers($mysql, $pgsql, $queries);
-//doOrders($mysql, $pgsql, $queries);
+doOrders($mysql, $pgsql, $queries);
